d27efba63d6e3e93b2cc83da80e88c582f947684
Liidia Laada <liidia@sixfold.com>
2022-08-18 10:44:03 +0300

    Implement core game logic, use arrow functions

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function () {
+const getPlayerChoice = () => {
   const selection = prompt(
     `${ROCK}, ${PAPER} or ${SCISSORS}?`,
     ''
@@ -21,7 +24,27 @@ const getPlayerChoice = function () {
   return selection;
 };
 
-startGameBtn.addEventListener('click', function () {
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = (cChoice, pChoice) =>
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
+
+startGameBtn.addEventListener('click', () => {
   if (gameIsRunning) {
     return;
   }
@@ -29,7 +52,10 @@ startGameBtn.addEventListener('click', function () {
   gameIsRunning = true;
   console.log('Game is starting...');
 
-  const playerSelection = getPlayerChoice();
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
 
-  console.log(playerSelection);
+  const winner = getWinner(computerChoice, playerChoice);
+
+  console.log(winner);
 });
